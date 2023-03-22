a5a7f56cc3dcf0fa883f604a73188ddb3e23ef39
Gavin Sargent <gavin.sargent3535@gmail.com>
2023-03-22 16:40:33 -0600

    Added image and some CSS to site

--- public/index.js
+++ public/index.js
@@ -1,5 +1,11 @@
 const btn = document.getElementById('hairstyle-button')
+const mohawkImg = document.getElementById('mohawk-guy')
 
-const hairstyleBtn = () => alert('Do a Mohawk!')
+
+const hairstyleBtn = () => {
+    alert('Do a Mohawk!')
+    mohawkImg.style.visibility = 'visible'
+}
+
 
 btn.addEventListener('click', hairstyleBtn)
